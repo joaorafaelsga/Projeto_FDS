ded03b72afe3cd5c217f48ec4a83ff2f20444fa7
Marco Veras <marco.sansonesouza@hotmail.com>
2026-09-18 10:47:01 -0300

    Cria modelo inicial de empresa

--- esg/models.py
+++ esg/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=150)
+    setor = models.CharField(max_length=100)
+    porte = models.CharField(max_length=50)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
